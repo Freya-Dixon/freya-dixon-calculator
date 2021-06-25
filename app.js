eb10c8bd7ae8f5ad1130ce94e74bf74eef6d13dc
Freya-Dixon <freya.dixon@hotmail.com>
2021-06-25 18:17:02 +0100

    added and console logged additional operator buttons

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 //access html elements onto js 
 
 const numberResult = document.querySelector('h1');
+console.log(numberResult);
 const numberButtons = document.querySelectorAll('.btn.number');
 console.log(numberButtons);
+
 const allButtons = document.querySelectorAll('.btn');
+console.log(allButtons);
 
 const clearButton = document.querySelector('.btn.function.clear');
 console.log(clearButton);
 
-const plusButton = document.querySelector('.btn.function.minus');
-console.log(plusButton);
 
 const functionsButtons = document.querySelectorAll('.btn.function')
+console.log(functionsButtons);
 
 const operatorButtons = document.querySelectorAll('.btn.operator')
+console.log(operatorButtons);
+
+const plusButton = document.querySelector('.btn.function.plus');
+console.log(plusButton);
+
+const equalButton = document.querySelector('.btn.operator.equals')
+console.log(equalButton);
+
+const myStorage = window.sessionStorage;
+console.log(myStorage);
 
 allButtons.forEach((button) => {
     button.addEventListener('click', (e)=> {
@@ -29,40 +41,40 @@ let value = 0;
     } else {
         value = value + e.target.innerHTML
         numberResult.innerHTML = value;
+
     }
-    clearButton.addEventListener('click',() => {
-        numberResult.innerHTML = 0;
-    });
 }
 
-//clears ouput 
+// clears output- should be able to clear value without keeping session storage data and display 0 on H1
 
 // clearButton.addEventListener('click',() => {
 //     numberResult.innerHTML = 0;
+    //    sessionStorage.removeItem('');
 // });
+// 
 
 
 // operator interaction 
 
 // plus function 
 
-const sum = () => {
+const sum = (value) => {
+    value + value
+}
+
+// const multiply = () => {
     
-}
+// }
 
-const multiply = () => {
+// const minus = () => {
+
+// }
+
+// const divide = () => {
+
+// }
+
+// const findPercentage = () => {
     
-}
-
-const minus = () => {
-
-}
-
-const divide = () => {
-
-}
-
-const findPercentage = () => {
-    
-}
+// }
 
